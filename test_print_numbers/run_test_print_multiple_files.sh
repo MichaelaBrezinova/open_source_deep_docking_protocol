76a9109fb9504f47575d88ac4a9551135db6e584
Michaela Brezinova <mb2462@login-q-1.data.cluster>
2022-12-05 17:32:17 +0000

    apply git ignore

--- test_print_numbers/run_test_print_multiple_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in {0..3}; 
-do sbatch --account=VENDRUSCOLO-SL3-CPU --partition=skylake --nodes=1 --ntasks=1 --cpus-per-task=1 --time=00:10:00 --wrap "python test_print_numbers.py -file_num $i"; 
-done
